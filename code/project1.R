eff360ebb1c4d1b8df6beb7030d666aea827fd62
Eric <heimleiter.dasheim@gmail.com>
2024-05-28 00:47:55 +0200

    Display sheet

--- code/project1.R
+++ code/project1.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(colorspace)
 library(grDevices)
 library(tidyr)
+library(gridExtra)
 
 lab_colors <- read.csv2("data/LabMeasurements-Color-Card.csv")
 # every observation is one color card
@@ -38,7 +39,7 @@ lab_to_rgb <- function(l, a, b) {
   rgb(convertColor(c(l, a, b), from = "Lab", to = "sRGB"))
 }
 
-display_color_card <- function(df, row_name = "Row", col_name = "Col", color_space = NaN, space_between = 1) {
+display_color_card <- function(df, row_name = "Row", col_name = "Col", color_space = NaN, space_between = 1, circle_size=21) {
   if (!all(c(row_name, col_name) %in% colnames(df))) {
     stop("Dataframe must contain " + row_name + " and " + col_name + " columns")
   }
@@ -62,14 +63,36 @@ display_color_card <- function(df, row_name = "Row", col_name = "Col", color_spa
   
   ggplot() +
     geom_tile(data = df_no_corners, aes_string(x = col_name, y = row_name, fill = "Color"), color = "white", size = space_between) +
-    geom_point(data = corners, aes_string(x = col_name, y = row_name, fill = "Color"), shape = 21, color = "white", size = 20) +
+    geom_point(data = corners, aes_string(x = col_name, y = row_name, fill = "Color"), shape = 21, color = "white", size= circle_size) +
     scale_fill_identity() +
     theme_void() +
     coord_fixed()
 }
 
+display_color_sheet <- function(df, color_sheet_idx = 1) {
+  df_sheet <- lab_colors %>% filter(Sheet == color_sheet_idx)
+  plots <- list()
+  
+  for (i in 1:nrow(df_sheet)) {
+    row <- df_sheet[i, ]
+    tiles <- data.frame()
+    for (r in 1:8) {
+      for (c in 1:8) {
+        lab <- as.numeric(unlist(row[paste0(c("L", "a", "b"), r, c)]))
+        tiles <- rbind(tiles, data.frame(Row = r, Col = c, L = lab[1], a = lab[2], b = lab[3]))
+      }
+    }
+    plots[[i]] <- display_color_card(tiles, row_name = "Row", col_name = "Col", color_space = "Lab", circle_size=3)
+  }
+  
+  do.call(grid.arrange, c(plots, ncol = 6))
+}
+
 display_color_card(master_colors, "Crow", "Ccol", "CMYK", 2)
 display_color_card(master_colors, "Crow", "Ccol", "Lab", 2)
-# color sheet display function 
-# how does the column S in master influence CMYK values
 
+display_color_sheet(lab_colors, color_sheet_idx = 1)
+display_color_sheet(lab_colors, color_sheet_idx = 5)
+display_color_sheet(lab_colors, color_sheet_idx = 13)
+
+# how does the column S in master influence CMYK values
